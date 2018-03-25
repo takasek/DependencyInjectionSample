b98c1acfb59fdedd2555b99d3be2edd4fb14ad69
Yoshitaka Seki <takassekiyoshi@gmail.com>
2018-03-25 17:12:38 +0900

    Write logic on ViewController

--- DependencyInjectionSample/ViewController.swift
+++ DependencyInjectionSample/ViewController.swift
@@ -9,15 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let lastDate: Date?
+        let lastTimeIntervalSince1970 = UserDefaults.standard.double(forKey: "lastTimeIntervalSince1970")
+        if lastTimeIntervalSince1970 != 0 {
+            lastDate = Date(timeIntervalSince1970: lastTimeIntervalSince1970)
+        } else {
+            lastDate = nil
+        }
+        let now = Date()
+
+        label.text = timeDescription(from: lastDate, to: now)
+
+        UserDefaults.standard.set(now.timeIntervalSince1970, forKey: "lastTimeIntervalSince1970")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func timeDescription(from lastDate: Date?, to now: Date) -> String {
+        let f = DateFormatter()
+        f.setLocalizedDateFormatFromTemplate("kHms")
+
+        return String(
+            format: "last: %@\nnow: %@",
+            lastDate.flatMap(f.string(from:)) ?? "--",
+            f.string(from: now)
+        )
     }
 }
 
