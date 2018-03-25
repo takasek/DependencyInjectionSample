79f758e75e3d92ae65ac96a5a216764cbd0fe246
Yoshitaka Seki <takassekiyoshi@gmail.com>
2018-03-25 16:55:36 +0900

    add Xcode Project

---
+++ DependencyInjectionSample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DependencyInjectionSample
+//
+//  Created by Yoshitaka Seki on 2018/03/25.
+//  Copyright © 2018年 takasek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
